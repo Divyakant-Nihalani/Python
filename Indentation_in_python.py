43cc221d6fa7db2d790245c9e0ef9e0a676a1287
ba063330 <divyakant-jayprakash.nihalani@stud.uni-bamberg.de>
2020-03-27 12:48:09 +0100

    Basic concepts of python

---
+++ Indentation_in_python.py
@@ -0,0 +1,4 @@
+a=5
+b=10
+if a<b:
+    print(a) #4 space indentation is used instead of curly brackets
---
+++ list.py
@@ -0,0 +1,9 @@
+mylist = []
+mylist.append(1)
+mylist.append("hello")
+for x in mylist:
+    print(x)
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
